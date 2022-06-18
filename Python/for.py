1009bf5776ed2be64888128d409aa5b50dd8b4cb
danlhennessy <danh118@hotmail.co.uk>
2022-06-18 23:04:59 +0100

    list comprehension

--- Python/for.py
+++ Python/for.py
@@ -8,10 +8,18 @@ for index in numbers:
         
     
 
-
+#Modifying Original list (not recommended?)
 numbers2 = [1,2,3,4,5,6,7,8,9,10]
 
 for i in range(len(numbers2)):
     numbers2[i] += 2
 
 print(numbers2)
+
+#List Comprehension
+numbers3 = [1,2,3,4,5,6,7,8,9,10]
+newnumbers3 = []
+
+for i in numbers3:
+    newnumbers3.append(i+30)
+print(newnumbers3)
