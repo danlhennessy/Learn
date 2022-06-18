020303b353256e8f0e3ad8a85b215e0b119ffe8f
danlhennessy <danh118@hotmail.co.uk>
2022-06-18 22:21:56 +0100

    passgen

--- Python/passgenerator.py
+++ Python/passgenerator.py
@@ -1,1 +1,9 @@
+import random
+import string
 
+def password_gen():
+    length = random.randint(10, 15)
+    for letter in password:
+        letter = random.choices(string.ascii_uppercase + string.digits)
+    print(password)
+password_gen()
