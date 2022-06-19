2d52060941847608f528ed8251e02b18b0378657
danlhennessy <danh118@hotmail.co.uk>
2022-06-19 14:25:36 +0100

    list

---
+++ Python/list.py
@@ -0,0 +1,19 @@
+list = [1,2,3,4,5,6,7]
+
+#Print first item in list
+print(list[0])
+
+list[0] += 3
+print(list[0])
+
+grid = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [0]
+]
+#print item in 1st row, 1st column
+print(grid[0][0])
+
+#Print item in 2nd row, 3rd column
+print(grid[1][2])
