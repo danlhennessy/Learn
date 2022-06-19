72b5acfad4ce8c77ef477386bb5b1ba9f03239dc
danlhennessy <danh118@hotmail.co.uk>
2022-06-19 16:21:10 +0100

    import

--- Python/basics/importmodules.py
+++ Python/basics/importmodules.py
@@ -1,3 +1,3 @@
 #Install modules from https://docs.python.org/3/py-modindex.html or third party modules, or from local py files(e.g. import random or import usefulscripts.py)
 
-#Third party modules, generally can install with pip. e.g. pip install python-docx
+#Third party modules, generally can install with pip. e.g. pip install python-docx, then import using folder name e.g. dox
