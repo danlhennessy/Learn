54ca88a643fe688a00400966819e21394eb2ad4f
danlhennessy <danh118@hotmail.co.uk>
2022-06-19 16:40:01 +0100

    classfull

--- Python/basics/class.py
+++ Python/basics/class.py
@@ -6,4 +6,10 @@ class student:
         self.major = major
         self.gpa = gpa
         self.is_on_probation = is_on_probation
-        
+#Creating a 'student' object from the 'student' class       
+student1 = student("Dan", "Azure", 3.1, False)
+student2 = student("Pam", "Art", 3.4, True)
+
+print(student1.name)
+
+print(student2.gpa)
