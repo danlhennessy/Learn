60f6d0d71af82ba8b459ec95309d149538566d33
danlhennessy <danh118@hotmail.co.uk>
2022-06-19 13:35:48 +0100

    ui

---
+++ Python/pyui/testd.py
@@ -0,0 +1,10 @@
+import ui
+
+def addone(sender):
+	sender.title = 'hello!!'
+
+
+v = ui.load_view()
+v.present('sheet')
+
+
---
+++ Python/tkinter/tktest.py
@@ -0,0 +1,8 @@
+import tkinter
+
+top = tkinter.Tk()
+top.mainloop()
+
+
+#if __name__ == '__main__':
+    #main()
