25c17ba3bd1da982c27ef6068dbe53c179a92495
danlhennessy <danh118@hotmail.co.uk>
2022-06-20 18:17:23 +0100

    Quiz

---
+++ Python/games/Quiz.py
@@ -0,0 +1,28 @@
+
+
+question_prompts = [
+    "What color are Apples? \n (a) Red/Green \n(b) Purple\n(c) Orange \n\n",
+    "What color are Bananas? \n (a) Teal \n(b) Magenta\n(c) Yellow \n\n",
+    "What color are Strawberries? \n (a) Yellow \n(b) Red\n(c) Blue \n\n"                   
+                    ]
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+        
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+    
+]
+
+def test():
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score +=1
+    print(f"Thanks for completeing this Quiz, your score is {score}!")
+test()
