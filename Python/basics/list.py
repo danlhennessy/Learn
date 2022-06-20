c91d8712a1520ed099d6367aef7c329e9b8ac3ca
danlhennessy <danh118@hotmail.co.uk>
2022-06-20 19:00:34 +0100

    listsmore

--- Python/basics/list.py
+++ Python/basics/list.py
@@ -1,10 +1,10 @@
-list = [1,2,3,4,5,6,7]
+list1 = [1,2,3,4,5,6,7]
 
 #Print first item in list
-print(list[0])
+print(list1[0])
 
-list[0] += 3
-print(list[0])
+list1[0] += 3
+print(list1[0])
 
 grid = [
         [1, 2, 3],
@@ -17,3 +17,13 @@ print(grid[0][0])
 
 #Print item in 2nd row, 3rd column
 print(grid[1][2])
+
+
+emptylist = list()
+print(emptylist)
+
+#Lists allow different data types together
+list3 = [5, True, "Apple"]
+
+#Print last item in list
+print(list3[-1])
