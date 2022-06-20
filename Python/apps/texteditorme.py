01ef640382236e25f4bc531f958154e56d8665c6
danlhennessy <danh118@hotmail.co.uk>
2022-06-20 23:41:20 +0100

    filemenu

--- Python/apps/texteditorme.py
+++ Python/apps/texteditorme.py
@@ -18,6 +18,15 @@ text_scroll.pack(side=RIGHT, fill=Y)
 my_text = Text(my_frame, width=100, height=20, fg="blue", undo=True, yscrollcommand=text_scroll.set)
 my_text.pack()
 
+#Scroll Bar Config
 text_scroll.config(command=my_text.yview)
+
+#Menu Bar
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+#Menu - File
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu)
 
 root.mainloop()
