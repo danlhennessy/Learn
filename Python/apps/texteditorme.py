9a401447222d1aa306f4132ac3f7afd47c113f9d
danlhennessy <danh118@hotmail.co.uk>
2022-06-20 23:51:19 +0100

    spacing + quit command

--- Python/apps/texteditorme.py
+++ Python/apps/texteditorme.py
@@ -4,18 +4,18 @@ from tkinter import filedialog
 
 root = Tk()
 root.title("PY Text Editor")
-root.geometry("900x900")
+root.geometry("900x600")
 
 #Frame
 my_frame = Frame(root)
-my_frame.pack(pady=5)
+my_frame.pack(pady=5, padx=5)
 
 #Scroll Bar
 text_scroll = Scrollbar(my_frame)
 text_scroll.pack(side=RIGHT, fill=Y)
 
 #Text Box
-my_text = Text(my_frame, width=100, height=20, fg="blue", undo=True, yscrollcommand=text_scroll.set)
+my_text = Text(my_frame, width=120, height=120, fg="blue", undo=True, yscrollcommand=text_scroll.set)
 my_text.pack()
 
 #Scroll Bar Config
@@ -32,7 +32,7 @@ file_menu.add_command(label="New")
 file_menu.add_command(label="Open")
 file_menu.add_command(label="Save")
 file_menu.add_command(label="Save As")
-file_menu.add_command(label="Quit")
+file_menu.add_command(label="Quit", command=root.quit)
 
 #Menu - Edit
 edit_menu = Menu(my_menu, tearoff=False)
