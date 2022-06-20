31a8232c9ce0a14bf0b1eb4ef8f321591ae63fca
danlhennessy <danh118@hotmail.co.uk>
2022-06-20 18:24:08 +0100

    funcexplained

--- Python/basics/function.py
+++ Python/basics/function.py
@@ -2,3 +2,10 @@ def saysomething():
     print("something")
     
 saysomething()
+
+#Require 'name' as a parameter when calling function 'say_hi'
+def say_hi(name):
+    print(f"Hello {name}")
+
+#'dan' is the argument passed into the function    
+say_hi("dan")
