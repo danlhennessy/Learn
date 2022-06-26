2ecf91010ad1d84053de99bbcaac1dc8be9babef
danlhennessy <danh118@hotmail.co.uk>
2022-06-26 15:28:53 +0100

    linkedlist better + iterating

--- Python/Data Structures & Algorithms/linkedlistprac.py
+++ Python/Data Structures & Algorithms/linkedlistprac.py
@@ -1,10 +1,2 @@
-from linkedlists import Node, LinkedList
+from linkedlists import node, linkedlist
 
-mylist = LinkedList()
-mylist.add(5)
-mylist.add(8)
-mylist.add(12)
-mylist.remove(8)
-print(mylist.find(5))
-print(mylist.remove(12))
-print(mylist)
