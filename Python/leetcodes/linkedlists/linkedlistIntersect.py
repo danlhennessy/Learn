4d1aceeaf4260b876729d69b9b64a3590b9578d1
danlhennessy <danh118@hotmail.co.uk>
2022-07-02 22:08:38 +0100

    ll intersect

--- Python/leetcodes/linkedlists/linkedlistIntersect.py
+++ Python/leetcodes/linkedlists/linkedlistIntersect.py
@@ -5,3 +5,15 @@
 #         self.val = x
 #         self.next = None
 
+def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a, b = headA, headB
+        while a != b:
+            if a:
+                a = a.next
+            else:
+                a = headB #Once pointer a reaches the end, concatenate the two ll together
+            if b:
+                b = b.next
+            else:
+                b = headA #Once pointer b reaches the end, concatenate the two ll together
+        return a
