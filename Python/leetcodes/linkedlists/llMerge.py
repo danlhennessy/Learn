98125b1b1631c6d957b7b1f3e62ebedbbb61b520
danlhennessy <danh118@hotmail.co.uk>
2022-07-02 23:44:45 +0100

    linked list merge

---
+++ Python/leetcodes/linkedlists/llMerge.py
@@ -0,0 +1,24 @@
+#You are given the heads of two sorted linked lists list1 and list2.
+
+#Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+#Return the head of the merged linked list.
+
+def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+            
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
+        return dummy.next
--- Python/test/ttestt.py
+++ Python/test/ttestt.py
@@ -1,1 +1,2 @@
 #print(dir(int))
+
