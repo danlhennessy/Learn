ecea8224144aa702258443be0bdc76eb68cab455
danlhennessy <danh118@hotmail.co.uk>
2022-07-05 15:17:39 +0100

    str loop

--- Python/test/ttestt.py
+++ Python/test/ttestt.py
@@ -1,4 +1,11 @@
 #print(dir(int))
 
-for i in range(0, 21):
-    print(22 - 1- i, i)
+string = "Hello i am here"
+
+string1 = ""
+for v in string:
+    if v == " ":
+        string1 += "-"
+    else:
+        string1 += v
+print(string1)
