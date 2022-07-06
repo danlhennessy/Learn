21b67b467a3a6551b3e449aa14ebdac3bdccfd85
danlhennessy <danh118@hotmail.co.uk>
2022-07-06 08:33:00 +0100

    flask + html

--- Python/apps/flask/app/routes.py
+++ Python/apps/flask/app/routes.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'Username': 'Danec'}
+    return '''
+    <html>
+        <head>
+            <title>Home Page - Flasked</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['Username'] + '''!</h1>
+        </body>
+    </html>
+    '''
