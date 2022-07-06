d2998b1c02ddb6a9462ab0ea0026c9558f43ecec
danlhennessy <danh118@hotmail.co.uk>
2022-07-06 08:42:32 +0100

    flask html template

--- Python/apps/flask/app/routes.py
+++ Python/apps/flask/app/routes.py
@@ -3,14 +3,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'Username': 'Danec'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Flasked</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['Username'] + '''!</h1>
-        </body>
-    </html>
-    '''
+    user = {'username': 'Dan'}
+    return render_template('index.html', title='Home', user=user)
