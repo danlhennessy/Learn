0eb0e48f4122fd0a3d509763db9db9a1ecc9146c
danlhennessy <danh118@hotmail.co.uk>
2022-07-07 17:51:10 +0100

    using generators

--- Python/fundamentals/generators yield.py
+++ Python/fundamentals/generators yield.py
@@ -1,7 +1,17 @@
-def generator():
+#Generators are functions that return objects which can be iterated over. They are memory efficient for large data sets
+
+def mygenerator():
     yield 1
     yield 2
     yield 3
     
-for v in generator():
+for v in mygenerator():
     print(v)
+
+g = mygenerator()
+
+value = next(g)
+print(value) # Prints the next value in object g and pauses as that stage
+
+value = next(g)
+print(value)
