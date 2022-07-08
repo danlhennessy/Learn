fb93c3256bdcd19f099dec8795b27ee4b3770174
danlhennessy <danh118@hotmail.co.uk>
2022-07-08 22:21:25 +0100

    bs parse

--- Python/tools/webscrape/allsidesscrape.py
+++ Python/tools/webscrape/allsidesscrape.py
@@ -1,7 +1,23 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'https://www.allsides.com/media-bias/media-bias-ratings'
 
 r = requests.get(url)
 
-print(r.content[:100])
+soup = BeautifulSoup(r.content, "html.parser")
+
+#select_one('body') gets the body element
+#select_one('.temp') to get <a class="temp"></a>
+#select_one('#temp') to get <a id="temp"></a>
+#select_one('.temp a') to get <div class="temp"><a></a></div>
+#select_one('.temp.example') to get <a class="temp example"></a>
+
+rows = soup.select('tbody tr')
+
+row = rows[0]
+name = row.select_one('.views-field-title')
+namestripped = row.select_one('.views-field-title').text.strip()
+print(name)
+print(namestripped)
+
