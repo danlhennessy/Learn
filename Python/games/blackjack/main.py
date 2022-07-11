9b3cab91899e459bb0611371d16f9dc3723b8852
danlhennessy <danh118@hotmail.co.uk>
2022-07-11 12:05:02 +0100

    subclasses for suits

--- Python/games/blackjack/main.py
+++ Python/games/blackjack/main.py
@@ -2,6 +2,7 @@ import random
 
 
 cardtypes = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": [1, 11]}
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 class card:
     
     def __init__(self):
@@ -13,19 +14,23 @@ class card:
         print(cardtypes[card]) #Value of card
         
 class club(card):
+    suit = "Hearts"
     def __init__(self):
         super().__init__()
 class spade(card):
+    suit = "Hearts"
     def __init__(self):
         super().__init__()
 class diamond(card):
+    suit = "Hearts"
     def __init__(self):
         super().__init__()
 class heart(card):
+    suit = "Hearts"
     def __init__(self):
         super().__init__()
         
-heart1 = heart()
+
 
 card1 = card()
 
