483c0a06134e2315b33fed25008a2e7ff7d5f276
danlhennessy <danh118@hotmail.co.uk>
2022-07-11 21:05:09 +0100

    feeding a modified object as a parameter

--- Python/games/blackjack/main.py
+++ Python/games/blackjack/main.py
@@ -14,19 +14,7 @@ class blackjack:
         curcard = self.deck.drawcard()
         curcard.showcard()
         self.player.curval += curcard.value
-        while self.player.curval < 21:
-            choice = self.player.turn()
-            if choice.lower == "stick":
-                print(f"Your score is {self.curval}")
-            if choice.lower() == "twist":
-                curcard = self.deck.drawcard()
-                curcard.showcard()
-                self.player.curval += curcard.value
-                self.player.showval()
-        if self.player.curval == 21:
-            print("BlackJack!!!")
-        if self.player.curval > 21:
-            print("Bust!")
+        self.player.turn(self.deck)
 
 
 
--- Python/games/blackjack/player.py
+++ Python/games/blackjack/player.py
@@ -3,8 +3,20 @@ class player:  # player attributes, player turn, draw, showhand
     def __init__(self, curval):
         self.curval = curval
 
-    def turn(self):
-        return input("Stick or Twist: ")
+    def turn(self, deck):
+        while self.curval < 21:
+            choice = input("Stick or Twist: ")
+            if choice.lower == "stick":
+                print(f"Your score is {self.curval}")
+            if choice.lower() == "twist":
+                curcard = deck.drawcard()
+                curcard.showcard()
+                self.curval += curcard.value
+                self.showval()
+        if self.curval == 21:
+            print("BlackJack!!!")
+        if self.curval > 21:
+            print("Bust!")
         
     def showval(self):
         print(self.curval)
