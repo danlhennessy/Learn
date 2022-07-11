baf216385cfe7af6286df718b77f702975e825a1
danlhennessy <danh118@hotmail.co.uk>
2022-07-11 11:48:23 +0100

    card funcs

--- Python/games/blackjack/main.py
+++ Python/games/blackjack/main.py
@@ -1,27 +1,32 @@
 import random
 
+
+cardtypes = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": [1, 11]}
 class card:
-    def __init__(self,  type):
-        self.type = type
+    
+    def __init__(self):
+        pass
         
-    def dealCard():
+    def dealCard(self):
         card = random.choice(list(cardtypes.keys()))
-        print(card)
+        print(card) #Type of card
+        print(cardtypes[card]) #Value of card
         
 class club(card):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self):
+        super().__init__()
 class spade(card):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self):
+        super().__init__()
 class diamond(card):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self):
+        super().__init__()
 class heart(card):
-    def __init__(self, type):
-        super().__init__(type)
+    def __init__(self):
+        super().__init__()
         
-cardtypes = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": [1, 11]}
+heart1 = heart()
 
+card1 = card()
 
-    
+card1.dealCard()
