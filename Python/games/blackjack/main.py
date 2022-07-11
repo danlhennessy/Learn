ea5f4fdc40f53266500badb10b33750152459efe
danlhennessy <danh118@hotmail.co.uk>
2022-07-11 16:21:22 +0100

    rolling card values

--- Python/games/blackjack/main.py
+++ Python/games/blackjack/main.py
@@ -3,7 +3,7 @@ from deck import deck
 from card import card
 
 
-cardtypes = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": [1, 11]}
+cardtypes = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": 1}  # Fix Ace value later
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
 
@@ -20,11 +20,15 @@ class player:  # player attributes, player turn, draw, showhand
         newdeck = deck()
         newdeck.buildDeck()
         newdeck.shuffle()
+        curcard = newdeck.drawcard()
+        curcard.showcard()
+        self.curval += curcard.value
         choice = input("Stick or Twist: ")
         if choice.lower() == "stick":
             print(f"Your score is {self.curval}")
         if choice.lower() == "twist":
             curcard = newdeck.drawcard()
+            curcard.showcard()
             self.curval += curcard.value
             print(self.curval)
         else:
