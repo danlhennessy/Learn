1702ceafb34511293e316108c9ab7ff8042c0dad
danlhennessy <danh118@hotmail.co.uk>
2022-07-17 16:32:12 +0100

    bs4 templates

--- Python/tools/webscrape/bs4prac.py
+++ Python/tools/webscrape/bs4prac.py
@@ -1,5 +1,6 @@
-from requests_html import HTMLSession
 from bs4 import BeautifulSoup as bs
+from requests_html import HTMLSession
+
 
 # Get Data with requests-html
 url = "https://www.google.com/search?q=weather+London"
@@ -8,8 +9,11 @@ r = HTMLSession().get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0;
 # Define Parser
 soup = bs(r.text, 'html.parser')
 # Parse Text
-title = soup.find_all('title').text
-print(title)
+lis = soup.find_all('li')
+print(lis[0].text)
 # Parse Image
-
+image = soup.find('img', {'class': 'qIBnUc'})
+print(image['src'])
 # Parse link
+link = soup.find_all('a')
+print(link[2]['href'])
