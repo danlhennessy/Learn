0aacadfb550ceb88079545b0288bfbcdfa76a9a9
danlhennessy <danh118@hotmail.co.uk>
2022-07-18 21:25:27 +0100

    Numpy

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,21 +1,28 @@
-a = "Helloort"
-s = "aababcabc"
+phoneNumber = "3662277"
+words = ["foo", "bar", "baz", "foobar", "emo", "cap", "car", "cat"]
+
+# Convert word in words to phone digit
+# Iteration for checking substring
+
+def convert(mylist):
+    newlist = []
+    for word in mylist:
+        newword = word.replace('a', '2')
+        newlist.append(newword)
+    return(newlist)
 
 
 
-def goodsubs(s):
-    t, k = 0, 3
-    good = 0
-    while k < len(s) + 1:
-        myset = list(set(s[t:k]))
-        print(myset)
-        if len(myset) < 3:
-            t += 1
-            k += 1
-        else:
-            good += 1
-            t += 1
-            k += 1
-    print(good)
+def checknum(convlist):
+    anslist = []
+    convanslist = []
+    for word in convlist:
+        wordlist = list(word)
+        if wordlist in list(phoneNumber):
+            anslist.append(word)
+    for word in anslist:
+        newword = word.replace('2', 'a')
+        convanslist.append(newword)
+    print(convanslist)
 
-goodsubs(s)
+checknum(convert(words))
