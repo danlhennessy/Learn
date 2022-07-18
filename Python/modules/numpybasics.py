d7aa5bd5ddec3755a92ce6ae61fadb1ddfcbe1e5
danlhennessy <danh118@hotmail.co.uk>
2022-07-18 23:14:10 +0100

    numpy arrays

--- Python/modules/numpybasics.py
+++ Python/modules/numpybasics.py
@@ -1,3 +1,18 @@
 import numpy as np
 
 print(np.__version__)
+
+a = np.array([1,22,31,4,5,6])
+print(a)
+b = np.append(a, 25)
+c = np.insert(a, 3, 28) # Insert 28 at index 3
+print(a, b, c)
+if 25 in a:
+    print("25 Appended to a")
+if 25 in b:
+    print("25 only Appended to copy of a")
+if 28 in a:
+    print("25 only Inserted to copy of a")
+
+array6 = np.empty(6) # 6 Random digits, better than 6 0s for speed
+print(np.arange(2, 9, 2))
