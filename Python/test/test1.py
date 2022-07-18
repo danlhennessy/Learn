b1325be8823b2ba81249ee1d9aa4bd75eec74341
danlhennessy <danh118@hotmail.co.uk>
2022-07-18 15:09:59 +0100

    sliding window applied

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,3 +1,21 @@
 a = "Helloort"
+s = "aababcabc"
 
-print(a[:-1])
+
+
+def goodsubs(s):
+    t, k = 0, 3
+    good = 0
+    while k < len(s) + 1:
+        myset = list(set(s[t:k]))
+        print(myset)
+        if len(myset) < 3:
+            t += 1
+            k += 1
+        else:
+            good += 1
+            t += 1
+            k += 1
+    print(good)
+
+goodsubs(s)
