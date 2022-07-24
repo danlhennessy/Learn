69021b3784aec6489e81e7f0654575f3d58a2e27
danlhennessy <danh118@hotmail.co.uk>
2022-07-24 16:15:01 +0100

    catalan

---
+++ Python/recursion/catalan.py
@@ -0,0 +1,8 @@
+# Count the number of unique expressions containing n pairs of parentheses which are correctly matched. 
+# For n = 3, possible expressions are ((())), ()(()), ()()(), (())(), (()()).
+
+catalan_nums = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862]
+# Relationship: Cn = 2n! / ((n+1)!n!)
+# n = 1:   2! / 2!1! = 2/2 = 1
+# n = 2:   4! / 3!2! = 24/12 = 2
+    
