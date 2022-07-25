0c0d8298033811f6e4b8bbbf351c288c327e7896
danlhennessy <danh118@hotmail.co.uk>
2022-07-25 15:44:56 +0100

    google tests

--- Python/tools/webscrape/googleweathtest.py
+++ Python/tools/webscrape/googleweathtest.py
@@ -1,6 +1,7 @@
 from requests_html import HTMLSession
 import requests
 from bs4 import BeautifulSoup
+import json
 
 s = HTMLSession()
 
@@ -9,9 +10,11 @@ url = f"https://www.google.com/search?q=weather+{location}"
 
 r = s.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"})
 
-temp = print(r.html.find('span#wob_tm', first=True).text)
-
 r2 = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"})
-soup = BeautifulSoup(r.text)
-for image_src in soup.find_all("img"):
-    print(image_src['src'])
+soup = BeautifulSoup(r.content, "html.parser")
+
+temp = soup.find_all('text', 'wob_t.wob_gs_l0')
+# for image_src in soup.find_all("img"):
+    # print(image_src['src'])
+
+print(soup.json())
