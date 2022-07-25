3e4af8d6281af0483b4364a6d91f3d85340cff5c
danlhennessy <danh118@hotmail.co.uk>
2022-07-25 21:19:22 +0100

    testing request html

--- Python/tools/webscrape/googleweathtest.py
+++ Python/tools/webscrape/googleweathtest.py
@@ -13,8 +13,13 @@ r = s.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)
 r2 = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"})
 soup = BeautifulSoup(r.content, "html.parser")
 
-temp = soup.find_all('text', 'wob_t.wob_gs_l0')
-# for image_src in soup.find_all("img"):
-    # print(image_src['src'])
+temp = soup.find('text', 'wob_t wob_gs_l3')
 
-print(soup.json())
+r.html.render(sleep=10)
+
+weather = r.html.find('wob_t.wob_gs_l0', first=True)
+print(weather)
+
+
+svg = r.html.find('svg#wob_gsvg', first=True)
+print(svg.attrs)
