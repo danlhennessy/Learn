f92bc1077958335d5e5ecce43c59f17585c6e019
danlhennessy <danh118@hotmail.co.uk>
2022-07-27 11:29:42 +0100

    server connected to client

--- Python/socket/echoserver.py
+++ Python/socket/echoserver.py
@@ -6,6 +6,7 @@ PORT = 5050
 SERVER = '192.168.0.99'
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -16,11 +17,12 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-        print(f"[{addr}] {msg}")
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
     conn.close()
 
 
