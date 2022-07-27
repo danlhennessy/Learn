58f7fa6dcce7c78ff8a3283a2dc0ec73bee86ade
danlhennessy <danh118@hotmail.co.uk>
2022-07-27 11:21:48 +0100

    echo server

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,18 +1,6 @@
 
-mylist = []
+TEST = 5
 
+TEST += 2
 
-def loops(n):
-    if n in mylist:
-        print('Test')
-    elif n == 1:
-        return 'Test2'
-    elif n not in mylist and n != 1:
-        mylist.append(n)
-        x = str(n)
-        n = 0
-        for v in x:
-            n += (int(v) * int(v))
-        loops(n)
-
-loops(19)
+print(TEST)
