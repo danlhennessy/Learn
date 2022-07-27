ab7a35b8b952031fa9b4c401e9c24b23bc91b783
danlhennessy <danh118@hotmail.co.uk>
2022-07-27 23:47:54 +0100

    wraps module

--- Python/fundamentals/decorators.py
+++ Python/fundamentals/decorators.py
@@ -1,11 +1,11 @@
-# Decorators 
-
+from functools import wraps
+import time
 def decorator_func(original_func):
+    @wraps(original_func)  # Used to copy over original function name/arguments etc rather than the wrapper func when used as a decorator
     def wrapper_func(*args):
         print(f'Wrapper executed this before {original_func.__name__}')
         return original_func(*args)
     return wrapper_func
-
 class decorator_class(object):
     def __init__(self, original_func):
         self.original_func = original_func
@@ -14,11 +14,22 @@ class decorator_class(object):
         print(f'call method executed this before {self.original_func.__name__}')
         return self.original_func(*args)
 
-@decorator_func  # Whenever display() is called, decorator_func also runs with display as the argument
+def my_timer(original_func):
+    @wraps(original_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = original_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print(f'{original_func.__name__} ran in {t2}')
+        return result
+    return wrapper
+
+@decorator_class # Can use the __call__ method of a class to achieve same result as using a decorator function
 def display():
     print('Display Function Ran')
 
-@decorator_class # Can use the __call__ method of a class to achieve same result as using a decorator function
+@decorator_func # Whenever display() is called, decorator_func also runs with display as the argument
+@my_timer
 def display_info(name, age):
     print(f'display info ran with args ({name},{age})')
     
--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,6 +1,27 @@
+def isiso(s, t):
+    mydicts = {}
+    mydictt = {}
+    anslists = []
+    anslistt = []
+    for v in s:
+        if v not in mydicts:
+            mydicts[v] = 1
+            anslists.append(mydicts[v])
+        else:
+            mydicts[v] += 1
+            anslists.append(mydicts[v])
+    for v in t:
+        if v not in mydictt:
+            mydictt[v] = 1
+            anslistt.append(mydictt[v])
+        else:
+            mydictt[v] += 1
+            anslistt.append(mydictt[v])
+    print(anslists, anslistt)
+    print(mydicts, mydictt)
+    if anslists == anslistt:
+        return True
+    else:
+        return False
 
-TEST = 5
-
-TEST += 2
-
-print(TEST)
+print(isiso('bbbaaaba', 'aaabbbba'))
