0c1d4e8b559c765bce8799e9c0e7df02078329f3
danlhennessy <danh118@hotmail.co.uk>
2022-07-28 17:29:42 +0100

    binary search + regex start

--- Python/fundamentals/binarysearch.py
+++ Python/fundamentals/binarysearch.py
@@ -26,8 +26,26 @@ def binary_search(list, target, low=None, high=None):
     else: # target > list[midpoint]
         return binary_search(list, target, midpoint+1, high)
     
+def condition(num, target):
+    if num >= target:
+        return True
+    else:
+        return False
+
+def binary2(list, target):
+    left, right = 0, len(list)
+    while left < right:
+        mid = left + (right - left) // 2
+        if condition(mid, target):
+            right = mid
+        else:
+            left = mid + 1
+    return left  # left will be the minimal item in list to satisfy condition()
+    
 if __name__=='__main__':
     list = [1,2,3,4,5,6,7,8,24,25,26,27,28,34,35,36,37,38,45,46,47,48,49]
     target = 6
     print(naive_search(list, target))
+    print(binary2(list, target))
     print(binary_search(list, target))
+    
---
+++ Python/fundamentals/regex_pressions.py
@@ -0,0 +1,5 @@
+import re
+print('\tTab')
+print(r'\tTab')
+
+pattern = re.compile()
