b9a492be9db92ff9781ea104cafc6285ffdb2e77
danlhennessy <danh118@hotmail.co.uk>
2022-08-01 23:38:02 +0100

    cache / lru_cache

--- Python/fundamentals/Built In Modules/functools/main.py
+++ Python/fundamentals/Built In Modules/functools/main.py
@@ -1,4 +1,5 @@
 # functools - module for higher order functions (Functions that take other funcs as arguments OR return funcs as a result)
+from functools import cache, lru_cache
 import functools
 
 def add(a: float, b: float):
@@ -10,3 +11,17 @@ print(add_6(12))
 
 add_7 = functools.partial(add, b=7)  # Choose which arg to pass with arg=
 print(add_7(12))
+
+@lru_cache(maxsize=5)  # cache remembers previously computed numbers. Useful for avoiding slow repeated computations. lru_cache stores a set number of previous values
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+def main():
+    for i in range(400):
+        print(i, fib(i))
+    print("finished")
+    
+if __name__ == '__main__':
+    main()
