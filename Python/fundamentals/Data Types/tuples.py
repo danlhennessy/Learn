45dad325398d98303a9799a03b8cd8e117e05f1e
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 21:45:26 +0100

    * for creating iterables

--- Python/fundamentals/Data Types/tuples.py
+++ Python/fundamentals/Data Types/tuples.py
@@ -12,3 +12,5 @@ city, year, pop = ('Tokyo', 2003, 32_450)
 
 a, b, *rest = range(6)  # The * indicator, like with *args, is used in place of excess arguments/variables
 print(a,b,rest)
+
+print((*range(4), 4) == (0,1,2,3,4))  # The * can also be used on the right side of the definition, when creating iterables
