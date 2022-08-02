f8dc41b33e96bd2c1a73e8817ac9e9ccf07a14f9
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 15:18:34 +0100

    t

--- Python/fundamentals/Control Flow/genexps.py
+++ Python/fundamentals/Control Flow/genexps.py
@@ -5,5 +5,5 @@ symbols = '$¢£¥€¤'
 mytup = tuple(ord(symbol) for symbol in symbols)  # Creating a tuple from a generator expression
 print(mytup)
 
-for tshirt in (f'{c} {s}' for c in colours for s in sizes):
+for tshirt in (f'{c} {s}' for c in colours for s in sizes):  # Printing each tshirt as they are yielded using genexp
     print(tshirt)
