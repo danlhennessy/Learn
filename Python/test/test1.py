aaf1b1b292f7b3e9c751c8fcfe55971b9a8b4764
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 12:31:36 +0100

    overwritng special methods

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,22 +1,10 @@
-mynums = [1,11,3,0,15,5,2,4,10,7,12,6]
 
-
-def largestrange(nums):
-    sortedlist = sorted(nums)
-    last = sortedlist[0]
-    cursub = [last]
-    longestsub = []
-    for i, v in enumerate(sortedlist[1:]):
-        print(f'trying {v}')
-        if v == last + 1:
-            cursub += [v]
-            last = v
-            if len(cursub) > len(longestsub):
-                longestsub = cursub
-        else:
-            cursub = [v]
-        print(cursub)
-            
-    print([longestsub[0], longestsub[-1]])
-    
-largestrange(mynums)
+class testminus:
+    def __init__(self, name):
+        self.name = name
+    def __neg__(self):
+        return 45
+        
+test1 = testminus('John')
+one = -test1
+print(one)
