42a542ef66a88c05615221b09791cf4916e28035
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 12:52:45 +0100

    arithmetic vs unary

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,10 +1,15 @@
 
-class testminus:
-    def __init__(self, name):
-        self.name = name
-    def __neg__(self):
-        return 45
+class testnum:
+    def __init__(self, val):
+        self.val = val
+    def __sub__(self, other: int):
+        return self.val - other
+    def __add__(self, other):
+        return self.val + other
+    def __rsub__(self, other: int):
+        return other - self.val
         
-test1 = testminus('John')
-one = -test1
-print(one)
+test1 = testnum(28)
+one = test1 - 4
+add = test1 + 4
+print(add, one)
