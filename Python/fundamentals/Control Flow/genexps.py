820417153f309b96200f41f18cfc65ba61ca135a
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 15:17:52 +0100

    genexps

---
+++ Python/fundamentals/Control Flow/genexps.py
@@ -0,0 +1,9 @@
+# Generator Expressions - Similar to listcomps but for building other sequence types - tuples, dicts etc
+colours = ['red', 'blue', 'white']
+sizes = ['small', 'medium', 'large']
+symbols = '$¢£¥€¤'
+mytup = tuple(ord(symbol) for symbol in symbols)  # Creating a tuple from a generator expression
+print(mytup)
+
+for tshirt in (f'{c} {s}' for c in colours for s in sizes):
+    print(tshirt)
