6a6792dc4c05bd24dd7cf9e74079ce3e3b9ed1da
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 22:06:33 +0100

    slice class

---
+++ Python/fundamentals/Functions/slicing.py
@@ -0,0 +1,4 @@
+
+mystr = "Hello21mnfnfsd"
+two_seven = slice(2, 7)
+print(mystr[two_seven])
--- Python/test/test1.py
+++
@@ -1,6 +0,0 @@
-a = 1,2,3
-print(type(a))
-mytup = reversed(a)
-iter1 = iter(mytup)
-print(next(iter1))
-print(next(iter1))
