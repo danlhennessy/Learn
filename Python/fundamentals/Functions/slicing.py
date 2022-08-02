7f38ca94cbe6be4e7d0ac8fc99fac9db3911da3d
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 22:11:59 +0100

    slicing to change list inplace

--- Python/fundamentals/Functions/slicing.py
+++ Python/fundamentals/Functions/slicing.py
@@ -1,4 +1,8 @@
-
 mystr = "Hello21mnfnfsd"
 two_seven = slice(2, 7)
 print(mystr[two_seven])
+
+# Slicing to change mutable objects inplace:
+mylist = list(range(12))
+mylist[3:4] = 13, 19  # RHS must be an iterable
+print(mylist)
