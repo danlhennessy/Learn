fd86c8eac25403b259b25b9b148c4b7b34e2fd8e
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 15:23:53 +0100

    comment

--- Python/fundamentals/Data Types/tuples.py
+++ Python/fundamentals/Data Types/tuples.py
@@ -6,4 +6,4 @@ sizes = ['small', 'medium', 'large']
 products = [(c, s) for c in colours for s in sizes]
 
 for p in products:
-    print('%s / %s' % p)
+    print('%s / %s' % p)  # %s formatting takes a tuple, in this case p and returns its values in order.
