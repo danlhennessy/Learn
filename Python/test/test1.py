9c78e1327016a3de5440f6100127fd7eaa1c9636
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 13:14:56 +0100

    converting argument

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -4,12 +4,12 @@ class testnum:
         self.val = val
     def __sub__(self, other: int):
         return self.val - other
-    def __add__(self, other):
-        return self.val + other
+    def __add__(self, other: int):
+        return self.val + int(other)
     def __rsub__(self, other: int):
         return other - self.val
         
 test1 = testnum(28)
 one = test1 - 4
-add = test1 + 4
+add = test1 + '224'
 print(add, one)
