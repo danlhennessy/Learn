938d8205ddb3a130009e50cbd84d5aed40fa981e
danlhennessy <danh118@hotmail.co.uk>
2022-08-02 15:30:17 +0100

    tuple unpack

--- Python/fundamentals/Data Types/tuples.py
+++ Python/fundamentals/Data Types/tuples.py
@@ -6,4 +6,6 @@ sizes = ['small', 'medium', 'large']
 products = [(c, s) for c in colours for s in sizes]
 
 for p in products:
-    print('%s / %s' % p)  # %s formatting takes a tuple, in this case p and returns its values in order.
+    print('colour: %s / size: %s' % p)  # %s formatting takes a tuple, in this case p and returns its values in order. Its is an example of tuple unpacking
+# tuple unpacking:
+city, year, pop = ('Tokyo', 2003, 32_450)
