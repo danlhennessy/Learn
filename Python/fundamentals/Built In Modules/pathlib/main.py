de69e0322351a93df9729a779829b8cd791b4a91
danlhennessy <danh118@hotmail.co.uk>
2022-08-04 17:15:15 +0100

    iterate directory

--- Python/fundamentals/Built In Modules/pathlib/main.py
+++ Python/fundamentals/Built In Modules/pathlib/main.py
@@ -5,4 +5,8 @@ p = Path('.')  # Creates a path object in found OS (Windows Path)
 test = [x for x in p.iterdir() if x.is_dir()]
 print(p.resolve())  # Show file dir in your OS format (D:\Backup\Work\DevOps\Programming\Scripts\Python\fundamentals\Built In Modules\pathlib)
 new_p = p / 'Test dir'
-new_p.mkdir()  # Create folder at location p
+# new_p.mkdir()  # Create folder at location p
+for file_name in new_p.iterdir():
+    if file_name.match('*.txt') or file_name.match('*.py'):  # Check for specific file types when iterating through files in path
+        print(file_name)
+        
