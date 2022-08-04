e3845917fdbd315469f773f8212baa25ed44a19d
danlhennessy <danh118@hotmail.co.uk>
2022-08-04 17:06:07 +0100

    mkdir

--- Python/fundamentals/Built In Modules/pathlib/main.py
+++ Python/fundamentals/Built In Modules/pathlib/main.py
@@ -1,9 +1,8 @@
-from pathlib import Path # Pathlib - operations on file system paths
+from pathlib import Path # Pathlib - Working with file paths
 
-p = Path('.')
+p = Path('.')  # Creates a path object in found OS (Windows Path)
+
 test = [x for x in p.iterdir() if x.is_dir()]
-print(test)
-test2 = list(p.glob('**/*.py'))
-a,*rest = test2
-print(test2)
-print(a)
+print(p.resolve())  # Show file dir in your OS format (D:\Backup\Work\DevOps\Programming\Scripts\Python\fundamentals\Built In Modules\pathlib)
+new_p = p / 'Test dir'
+new_p.mkdir()  # Create folder at location p
