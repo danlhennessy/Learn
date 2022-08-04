5c95ce005edabcb9d8a9f2e6e7076e5dd843acae
danlhennessy <danh118@hotmail.co.uk>
2022-08-04 22:41:23 +0100

    more iterable comp

--- Python/fundamentals/Control Flow/listcomps.py
+++ Python/fundamentals/Control Flow/listcomps.py
@@ -4,3 +4,6 @@ print(mycomp)
 nums = range(25)
 test = [lastone := v for v in nums]  # := is a walrus operator, the variable is assigned a value and returned at the same time
 print(lastone)
+
+board = [['_'] * 3 for i in range(3)]
+print(board)
