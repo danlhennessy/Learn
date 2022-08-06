fe31bb16afe57adecec7b45702295affc6557320
danlhennessy <danh118@hotmail.co.uk>
2022-08-06 14:40:04 +0100

    setcomp, dictcomp

---
+++ Python/fundamentals/Control Flow/set dict comps.py
@@ -0,0 +1,5 @@
+mylist = [1,3,3,4,5,7,7,8]
+myset= {unique for unique in mylist}  # setcomp
+
+employees = [("dan", 29), ("Fred", 22), ("Emma", 31)]
+mydict = {name: age for name, age in employees}  # dictcomp
