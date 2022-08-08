7376b94defedb7bc5bdc761dcaede8073dbd9301
danlhennessy <danh118@hotmail.co.uk>
2022-08-08 11:32:30 +0100

    os module

---
+++ Python/fundamentals/Built In Modules/os/main.py
@@ -0,0 +1,7 @@
+import os
+
+os.chdir('D:/Backup/Work/DevOps/Programming/Scripts/Python/fundamentals/Built In Modules/os')
+print(os.getcwd())  # Current Working Directory
+print(os.listdir())
+
+os.mkdir('afolder')
