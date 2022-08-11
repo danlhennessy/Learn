e4051b469bf61e639e3ecca5c21294626e75f2a3
danlhennessy <danh118@hotmail.co.uk>
2022-08-11 16:16:54 +0100

    test logs

--- Python/fundamentals/Built In Modules/logging/logging_funcs.py
+++ Python/fundamentals/Built In Modules/logging/logging_funcs.py
@@ -2,6 +2,6 @@ import logging
 
 logging.debug('This is a debug message')
 logging.info('This is an info message')
-logging.warning('This is a warning message')
+logging.warning('This is a warning message')  # By default messages of level warning and above are logged
 logging.error('This is an error message')
 logging.critical('This is a critical message')
