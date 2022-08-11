ca7ff3f932733083a170d63091ec9f211c9aa475
danlhennessy <danh118@hotmail.co.uk>
2022-08-11 19:17:59 +0100

    subprocess

--- Python/fundamentals/Built_In_Modules/subprocess/main.py
+++ Python/fundamentals/Built_In_Modules/subprocess/main.py
@@ -1,4 +1,5 @@
 import subprocess
 
-subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe")
+# subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe")
 
+subprocess.call(['cmd', '/c', 'dir'])  # cmd.exe, run, dir
