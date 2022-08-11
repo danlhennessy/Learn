e36832743ee5ef0d43098c3ec04e160b9c74ef5e
danlhennessy <danh118@hotmail.co.uk>
2022-08-11 15:56:23 +0100

    logging / debugging

--- Python/fundamentals/Control Flow/set dict comps.py
+++ Python/fundamentals/Control Flow/set dict comps.py
@@ -1,5 +1,5 @@
 mylist = [1,3,3,4,5,7,7,8]
 myset= {unique for unique in mylist if unique < 5}  # setcomp
 
-employees = [("dan", 29), ("Fred", 22), ("Emma", 31)]
+employees = ("dan", 29), ("Fred", 22), ("Emma", 31)
 mydict = {name: age for name, age in employees}  # dictcomp
