81bafc839ce84917b51d0dfe8db7e54074a963cd
danlhennessy <danh118@hotmail.co.uk>
2022-08-11 19:20:18 +0100

    call pwsh, cmd, powershell

--- Python/fundamentals/Built_In_Modules/subprocess/main.py
+++ Python/fundamentals/Built_In_Modules/subprocess/main.py
@@ -2,4 +2,4 @@ import subprocess
 
 # subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe")
 
-subprocess.call(['cmd', '/c', 'dir'])  # cmd.exe, run, dir
+subprocess.call(['pwsh', '/c', 'ls'])  # pwsh.exe, run, ls
