f26b56753af1fbd1384af803394013049f733ccb
danlhennessy <danh118@hotmail.co.uk>
2022-08-11 17:01:58 +0100

    dataclass testing

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,4 +1,9 @@
-x, y, z, *rest = [1,2,3,4,5,6,7]
+from dataclasses import dataclass
+@dataclass
+class person:
+    n: str = "Default"
+    age: int = 22
+    
+fred = person("Fred", 45)
 
-print(x,y,z,rest)
-print(x,y,z,*rest)
+print(fred.age)
