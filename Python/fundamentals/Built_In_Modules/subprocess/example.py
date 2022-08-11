fcf5851a72916170d7d3121b090924f3eb785d0d
danlhennessy <danh118@hotmail.co.uk>
2022-08-11 20:03:40 +0100

    stdouts

---
+++ Python/fundamentals/Built_In_Modules/subprocess/example.py
@@ -0,0 +1,5 @@
+# Example script to be captured via subprocess from another script
+
+from random import randint
+
+print(randint(0, 5000))
--- Python/fundamentals/Built_In_Modules/subprocess/main.py
+++ Python/fundamentals/Built_In_Modules/subprocess/main.py
@@ -3,3 +3,11 @@ import subprocess
 # subprocess.Popen("C:\Program Files\Google\Chrome\Application\chrome.exe")
 
 subprocess.call(['pwsh', '/c', 'ls'])  # pwsh.exe, run, ls
+
+randnumber_process = subprocess.run(["python", "example.py"], capture_output=True)
+
+print(randnumber_process.stdout)
+
+magic_number_process = subprocess.run(["python", "magic_number.py"], capture_output=True)
+
+print(magic_number_process.stdout)
