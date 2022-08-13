fccd05d4c158d873cedbd57d8701022dc61a7111
danlhennessy <danh118@hotmail.co.uk>
2022-08-13 15:07:35 +0100

    working, uploaded

--- Python/tools/packages/test/setup.py
+++ Python/tools/packages/test/setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup
 
-with open("README.md", "r") as f:
+with open(r"README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='helloworld',  #  The name used when pip installing (pip install helloworld)
+    name='helloworld-danlh',  #  The name used when pip installing (pip install helloworld)
     version='0.0.1',
     description='Test say hello func',
     py_modules=["helloworld"],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
     classifiers=[
-                 "Programming Language :: Python :: 3"
-                 "License :: OSI Approved :: MIT License"
+                 "Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
                  "Operating System :: OS Independent"
-                 ]
+                 ],
     long_description=long_description,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
     install_requires = [
                         "blessings >= 1.7",
-                        ]
+                        ],
+    extras_require = {
+        "dev": [
+                "pytest >= 3.7"
+                ]
+    }
 )
