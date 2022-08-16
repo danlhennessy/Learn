f75cac5aafbee88d9924256457b8f62cd77236bc
danlhennessy <danh118@hotmail.co.uk>
2022-08-16 23:23:27 +0100

    rename

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,22 +1,3 @@
-import time
+from afterbasics import add_two
 
-image3 = (".gif", ".jpg", ".png")
-image4 = "jpeg"
-app = (".pdf", ".txt", ".zip")
-# else application/octet-stream
-
-def extensions(file):
-    if file.endswith(image3):
-        print(f"image/{file[-3:]}")
-    elif file.endswith(image4):
-        print(f"image/{file[-4:]}")
-    elif file.endswith(app):
-        print(f"application/{file[-3:]}")
-    else:
-        return("application/octet-stream")
-
-t1 = time.perf_counter()
-extensions('cat.jpeg')
-t2 = time.perf_counter()
-
-print(t2-t1)
+print(add_two(23))
