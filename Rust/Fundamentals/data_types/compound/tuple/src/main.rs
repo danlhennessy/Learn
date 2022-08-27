f51a435e6158b3a912caf0673c2f435e91b3f7bc
danlhennessy <danh118@hotmail.co.uk>
2022-08-27 23:22:39 +0100

    modifying tuple indices inplace

--- Rust/Fundamentals/data_types/compound/tuple/src/main.rs
+++ Rust/Fundamentals/data_types/compound/tuple/src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
-    let tup: (i32, bool, char) = (1, true, 's');  // (i32, bool, char) = this tuples type
+    let mut tup: (i32, bool, char) = (1, true, 's');  // (i32, bool, char) = this tuples type
     println!("{}", tup.0);  // print element in index 0 of tup
+
+    tup.0 = 10;
+    println!("{}", tup.0);
+    println!("{}", tup.1);
 }
