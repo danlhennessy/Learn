d80cea1d9e1fe43ef60edddb9074e8c5a8f484e7
danlhennessy <danh118@hotmail.co.uk>
2022-08-27 23:21:02 +0100

    referencing elements in tuple

--- Rust/Fundamentals/data_types/compound/tuple/src/main.rs
+++ Rust/Fundamentals/data_types/compound/tuple/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    let tup: (i32, bool, char) = (1, true, 's')  // (i32, bool, char) = this tuples types
+    let tup: (i32, bool, char) = (1, true, 's');  // (i32, bool, char) = this tuples type
+    println!("{}", tup.0);  // print element in index 0 of tup
 }
