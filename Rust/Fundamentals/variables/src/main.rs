c9462974732b02a18787ef983324d93174f2603b
danlhennessy <danh118@hotmail.co.uk>
2022-08-27 17:08:51 +0100

    name shadowing, scopes

--- Rust/Fundamentals/variables/src/main.rs
+++ Rust/Fundamentals/variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let mut x = 4;
     println!("x is: {}", x);
-    x = 5;
+
+    {
+        let x = 2;
+        println!("x is: {}", x);
+    }
+
+    x = x + 1;
     println!("x is: {}", x);
 }
