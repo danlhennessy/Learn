be2ee7ff238a773e23f8722649fce600f339f4d1
danlhennessy <danh118@hotmail.co.uk>
2022-08-27 23:10:46 +0100

    0/1 return to

--- Rust/Fundamentals/data_types/float/src/main.rs
+++ Rust/Fundamentals/data_types/float/src/main.rs
@@ -3,8 +3,5 @@ fn main() {
     println!("{}", floating_point)
 
     let true_or_false: bool = false;
-    let true_or_false: bool = 0;  // 0  can be used as a replacement for false for bool data
-
     let true_or_false: bool = true;
-    let true_or_false: bool = 1;  // 1 = true
 }
