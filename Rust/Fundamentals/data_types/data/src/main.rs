9e33d26a861bf189cbb1d2a2cef8e593cf4d2c47
danlhennessy <danh118@hotmail.co.uk>
2022-08-27 23:13:31 +0100

    rename project test

--- Rust/Fundamentals/data_types/data/src/main.rs
+++ Rust/Fundamentals/data_types/data/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let floating_point: f64 = 10.9;  // f64 is the default for floats
-    println!("{}", floating_point)
+    println!("{}", floating_point);
 
     let true_or_false: bool = false;
     let true_or_false: bool = true;
