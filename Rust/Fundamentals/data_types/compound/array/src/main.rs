8ab2f37cde7d6d972685e44f683a56329a69943a
danlhennessy <danh118@hotmail.co.uk>
2022-08-27 23:28:26 +0100

    array type example

--- Rust/Fundamentals/data_types/compound/array/src/main.rs
+++ Rust/Fundamentals/data_types/compound/array/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let arr = [1, 2, 3, 4, 5];  // All elements in an array must be the same type
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];  // All elements in an array must be the same type
     println!("{}", arr[4]);
 }
