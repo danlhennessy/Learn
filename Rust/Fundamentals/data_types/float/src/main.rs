966c603b4446b9f19d6b7e3a51bcbd4b840e5755
danlhennessy <danh118@hotmail.co.uk>
2022-08-27 23:09:17 +0100

    bool, 1/0 alternatives

--- Rust/Fundamentals/data_types/float/src/main.rs
+++ Rust/Fundamentals/data_types/float/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     let floating_point: f64 = 10.9;  // f64 is the default for floats
     println!("{}", floating_point)
+
+    let true_or_false: bool = false;
+    let true_or_false: bool = 0;  // 0  can be used as a replacement for false for bool data
+
+    let true_or_false: bool = true;
+    let true_or_false: bool = 1;  // 1 = true
 }
