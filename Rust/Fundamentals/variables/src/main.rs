531e70560841ac0258cd4dcc98e1e92da7062a90
danlhennessy <danh118@hotmail.co.uk>
2022-08-28 17:15:41 +0100

    multiple assignment, 1 line

--- Rust/Fundamentals/variables/src/main.rs
+++ Rust/Fundamentals/variables/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut x = 4;  // Default integer type is i32
-    println!("x is: {}", x);
+    let mut x = 4; let y = 5; // Default integer type is i32
+    println!("x y is: {} {}", x, y);
 
     {
         let x: i16 = 2;  // Specifying x to have the data type i16
