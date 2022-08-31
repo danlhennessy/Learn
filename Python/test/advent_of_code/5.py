bef4e451a1e8d8f6c8657a5c05260309bbafad0b
danlhennessy <danh118@hotmail.co.uk>
2022-08-31 23:52:23 +0100

    comment clarity

--- Python/test/advent_of_code/5.py
+++ Python/test/advent_of_code/5.py
@@ -9,8 +9,8 @@ point = namedtuple('point', ['x', 'y'])
 points_list, x_list, y_list = [], [], []
 
 for line in my_input:
-    x1, y1, x2, y2 = re.findall(r'\d+', line)
-    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    x1, y1, x2, y2 = re.findall(r'\d+', line)  # Grabbing x/y coords from string
+    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # Converting each to ints
     if x1 == x2 and y1 == y2:
         x_list.append(x1)
         y_list.append(y1)
@@ -19,7 +19,7 @@ for line in my_input:
         x_list.append(x1)
         y_list += list(range(min(y1, y2), max(y1, y2)))
         for v in list(range(min(y1, y2), max(y1, y2) + 1)):
-            points_list.append(point(x1, v))
+            points_list.append(point(x1, v))  # Append all the points from the x1/y1 => x2/y2 line
     if x1 != x2 and y1 == y2:
         y_list.append(y1)
         x_list += list(range(min(x1, x2), max(x1, x2)))
@@ -29,6 +29,6 @@ for line in my_input:
 # Ignoring cases where x1 != x2 and y1 != 2 for now
 
 
-two_or_more = [val for val, i in Counter(points_list).items() if i >= 2]
+two_or_more = [val for val, i in Counter(points_list).items() if i >= 2]  # Find unique items with a count of two or more in the entire list of points
 
 print(f"# Overlaps of two or more lines: {len(two_or_more)}")
