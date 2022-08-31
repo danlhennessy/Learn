cf8da9b32d2c5dee4aed3129632f17f3fcf563d0
danlhennessy <danh118@hotmail.co.uk>
2022-08-31 23:16:27 +0100

    updated conditions

--- Python/test/advent_of_code/5.py
+++ Python/test/advent_of_code/5.py
@@ -11,17 +11,21 @@ y_list = []
 
 for line in my_input:
     x1, y1, x2, y2 = re.findall(r'\d+', line)
-    if x1 == x2 or y1 == y2:
-        x_list.append(int(x1)), x_list.append(int(x2))
-        y_list.append(int(y1)), y_list.append(int(y2))
+    if x1 == x2 and y1 == y2:
+        x_list.append(int(x1))
+        y_list.append(int(y1))
+    if x1 == x2 and y1 != y2:
+        x_list.append(int(x1))
+        y_list += list(range(min(y1, y2), max(y1, y2)))
+    if x1 != x2 and y1 == y2:
+        y_list.append(int(y1))
+        x_list += list(range(min(x1, x2), max(x1, x2)))
+        
+# Ignoring cases where x1 != x2 and y1 != 2 for now
 
 for i in range(0, len(x_list), 2):
     plt.plot(x_list[i:i+2], y_list[i:i+2], 'ro-')
     
 # plt.show()
 
-
-
-bor, zor = x_list[0], x_list[1]
-
-print(max(zor, bor), min(zor, bor))
+print(x_list)
