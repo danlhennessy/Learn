32364d029800dd9aa4b91ef751f1d1290395a908
danlhennessy <danh118@hotmail.co.uk>
2022-09-02 15:05:21 +0100

    ** unpacking practice

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,1 +1,9 @@
-from PIL import Image
+one, two, *rest = [1,2,3,4,5,6,7,8,9,10]
+
+print(two, rest)
+
+some = {1: 'tree', 2: 'bush', 3: 'car', 4: 'road'}
+others = {1: 'trees', 2: 'bushes', 3: 'cars', 5: 'roads'}
+
+merged = {**some, **others}
+print(merged)
