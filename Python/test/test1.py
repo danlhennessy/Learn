e35a828dcfb380db0a06f7bc4c11d7c4cd8a5c43
danlhennessy <danh118@hotmail.co.uk>
2022-09-04 13:05:03 +0100

    defining a getitem dunder method

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -3,7 +3,10 @@ class my_container():
         self.values = values
     def __len__(self):
         return len(self.values)
+    def __getitem__(self, ind):
+        return self.values[ind]
         
 obj = my_container({1: "one", 2: "two", 3: "tthre"})
 
 print(len(obj))
+print(obj[3])
