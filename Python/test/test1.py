336500b461bc2a3f2a012a456335a0922782b9fc
danlhennessy <danh118@hotmail.co.uk>
2022-09-07 20:31:56 +0100

    more bool testing

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -7,8 +7,15 @@ if 10:
     print("This is also true")
 
 five = 5
-ten = None
+ten = 10
 print(five == ten)
 print(bool(ten))
 
 print(5.0 == 5)
+
+if ten:
+    print('ten')
+if ten is not None:
+    print('ten is not none')
+if ten is None:
+    print('ten is None ')
