124606a185d4c4a9f87d09bd2f8b88140609aa3b
danlhennessy <danh118@hotmail.co.uk>
2022-09-09 12:00:53 +0100

    getitem

--- Python/test/testOOP1.py
+++ Python/test/testOOP1.py
@@ -2,6 +2,7 @@ class myString(str):
     def __init__(self, string):
         self.string = string
         super().__init__()
+        
     def append(self, a):
         self.string += a
         return self.string
@@ -9,7 +10,12 @@ class myString(str):
     def pop(self, p):
         return self.string[:-p]
     
+    def __getitem__(self, index):
+        print(f'getting item at index {index}...')
+        return self.string[index]
+    
 testring = myString("Hellos")
 
 print(testring.pop(3))
 print(testring)
+print(testring[2])
