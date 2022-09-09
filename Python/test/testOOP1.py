9049004fd32349b7826d86bc78877867d69d8e99
danlhennessy <danh118@hotmail.co.uk>
2022-09-09 12:05:56 +0100

    cant assign when inheriting from str

--- Python/test/testOOP1.py
+++ Python/test/testOOP1.py
@@ -13,17 +13,9 @@ class myString(str):
     def __getitem__(self, index):
         print(f'getting item at index {index}...')
         return self.string[index]
-    
-    def __setitem__(self, index, value):
-        print(f'Changing index {index} from {self.string[index]} to {value}')
-        self.string[index] = value
-    
+
 testring = myString("Hellos")
 
 print(testring.pop(3))
 print(testring)
 print(testring[2])
-
-testring[2] = 'a'
-
-print(testring)
