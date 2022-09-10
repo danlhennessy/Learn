fa2bf7bd14073e0b04909919652214f78ca262a7
danlhennessy <danh118@hotmail.co.uk>
2022-09-10 19:39:39 +0100

    docstrings

--- Python/test/testOOP2.py
+++ Python/test/testOOP2.py
@@ -1,8 +1,10 @@
 class point:
     def __init__(self, x=0, y=0):
+        """Create a point, if no x or y values are provided, they are set to a default 0"""
         self.move(x, y)
         
     def move(self, x, y):
+        """Move the point by provided x and y values"""
         self.x += x
         self.y += y
         
@@ -10,6 +12,6 @@ mypoint = point(2,5)
 
 print(mypoint.x, mypoint.y)
 
-mypoint.move(12,29)
+mypoint.move(12, 29)
 
 print(mypoint.x, mypoint.y)
