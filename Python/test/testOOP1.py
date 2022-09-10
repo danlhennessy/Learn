a5e31deddd6e7353899e126e2351a2d4ce369700
danlhennessy <danh118@hotmail.co.uk>
2022-09-10 10:59:55 +0100

    overwriting built in exception

--- Python/test/testOOP1.py
+++ Python/test/testOOP1.py
@@ -2,20 +2,23 @@ class myString(str):
     def __init__(self, string):
         self.string = string
         super().__init__()
-        
+
     def append(self, a):
         self.string += a
         return self.string
 
     def pop(self, p):
         return self.string[:-p]
-    
+
     def __getitem__(self, index):
         print(f'getting item at index {index}...')
-        return self.string[index]
+        try:
+            return self.string[index]
+        except TypeError:
+            return 'Error: Use integers for string indices'
 
 testring = myString("Hellos")
 
 print(testring.pop(3))
 print(testring)
-print(testring[2])
+print(testring['g'])
