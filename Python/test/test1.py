36260478dde0f7e7e2cf6b755f50e9c11a2bc06c
danlhennessy <danh118@hotmail.co.uk>
2022-09-12 13:04:31 +0100

    this

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,21 +1,3 @@
+import this
 
-
-if 5:
-    print("This is true")
-    
-if 10:
-    print("This is also true")
-
-five = 5
-ten = 10
-print(five == ten)
-print(bool(ten))
-
-print(5.0 == 5)
-
-if ten:
-    print('ten')
-if ten is not None:
-    print('ten is not none')
-if ten is None:
-    print('ten is None ')
+print(this)
