62d64865484af93d64491e2d8586dad69c8f43ea
Dan Hennessy <danh118@hotmail.co.uk>
2022-09-17 16:58:53 +0100

    list comp

--- Python/modules/pydantic/practice.py
+++ Python/modules/pydantic/practice.py
@@ -1,5 +1,6 @@
 import json
 import pydantic
+from typing import List
 
 class Book(pydantic.BaseModel):
     
@@ -13,7 +14,9 @@ class Book(pydantic.BaseModel):
 def main() -> None:
     with open("Desktop/Programming/Learn/Python/modules/pydantic/example.json") as f:
         data = json.load(f)
-        print(data['glossary']['title'])
+        glossentry = data['glossary']['GlossDiv']['GlossList']['GlossEntry']
+        books: List[Book] = [Book(**item) for item in glossentry]
+        print(data['glossary']['GlossDiv']['GlossList']['GlossEntry'])
         
         
 if __name__ == "__main__":
