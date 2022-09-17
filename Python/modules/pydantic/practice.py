8bd073513499a1119ef60ba02338e732e8ff7fa6
Dan Hennessy <danh118@hotmail.co.uk>
2022-09-17 17:08:47 +0100

    pydantic class from json data

--- Python/modules/pydantic/practice.py
+++ Python/modules/pydantic/practice.py
@@ -1,22 +1,23 @@
 import json
 import pydantic
-from typing import List
+from typing import List, Optional
+
 
 class Book(pydantic.BaseModel):
-    
-    ID: str
-    SortAs: str
-    GlossTerm: str
-    Acronym: str
-    Abbrev: str
+    title: str
+    author: str
+    publisher: str
+    price: float
+    isbn_10: Optional[str]
+    isbn_13: Optional[str]
+    subtitle: Optional[str]
 
 
 def main() -> None:
     with open("Desktop/Programming/Learn/Python/modules/pydantic/example.json") as f:
         data = json.load(f)
-        glossentry = data['glossary']['GlossDiv']['GlossList']['GlossEntry']
-        books: List[Book] = [Book(**item) for item in glossentry]
-        print(data['glossary']['GlossDiv']['GlossList']['GlossEntry'])
+        books: List[Book] = [Book(**item) for item in data]
+        print(books)
         
         
 if __name__ == "__main__":
