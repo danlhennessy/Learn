729c117bae607f1796e38b93cbf14ceda39bd06f
Dan Hennessy <danh118@hotmail.co.uk>
2022-09-17 16:54:55 +0100

    model

--- Python/modules/pydantic/practice.py
+++ Python/modules/pydantic/practice.py
@@ -1,6 +1,14 @@
 import json
 import pydantic
 
+class Book(pydantic.BaseModel):
+    
+    ID: str
+    SortAs: str
+    GlossTerm: str
+    Acronym: str
+    Abbrev: str
+
 
 def main() -> None:
     with open("Desktop/Programming/Learn/Python/modules/pydantic/example.json") as f:
