f6e761a64c2af23af5145e5a3736055f60deccc0
Dan Hennessy <danh118@hotmail.co.uk>
2022-09-25 18:06:57 +0100

    default value

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -3,7 +3,10 @@ from dataclasses import dataclass
 @dataclass
 class country:
     capital: str
+    pop: int = 20_000
     
 Nigeria= country(capital = "Abuja")
 
 print(Nigeria.capital)
+
+print(Nigeria.pop)
