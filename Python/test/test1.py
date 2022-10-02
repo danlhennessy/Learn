ccc2d3db5c5f52556768a97499cc4d75952f08a4
Dan Hennessy <danh118@hotmail.co.uk>
2022-10-02 22:18:27 +0100

    function it daily prac..

--- Python/test/test1.py
+++ Python/test/test1.py
@@ -1,6 +1,13 @@
-a, b = 0, 1
 
-while b < 50:
-    a, b = b, a + b
-    print(f'a = {a}, b = {b}')
-    
+
+def fibbo():
+    """Print all fibonacci numbers below 50
+    """
+    a, b = 0, 1
+    while b + a < 50:
+        a, b = b, a + b
+        print(f'b = {b}')
+
+
+if __name__ == '__main__':
+    fibbo()
