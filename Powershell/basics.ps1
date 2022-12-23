a79443c47a119ea1230f88ebc2bdb856c40f6e27
danlhennessy <danh118@hotmail.co.uk>
2022-12-23 21:31:29 +0000

    ssh split

--- Powershell/basics.ps1
+++ Powershell/basics.ps1
@@ -1,11 +1,14 @@
+# User input + variable
 $var = Read-Host "Enter a number"
 echo $var
 
+# Consecutive commands
 Write-Output "command 1"; Write-Output "command 2"
 
-$string = 'tewst'
+
 
 # Pipe output of $string to Get-Member
+$string = 'tewst'
 $string | Get-Member
 
 #Using EndsWith method on $string
