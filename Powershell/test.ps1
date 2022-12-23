e9a77e541be656c6c5033d5f0501748dcb74d670
danlhennessy <danh118@hotmail.co.uk>
2022-12-23 18:16:48 +0000

    piping + methods

--- Powershell/test.ps1
+++ Powershell/test.ps1
@@ -1,4 +1,12 @@
 $var = Read-Host "Enter a number"
 echo $var
 
-Write-Output $env:USERNAME; pwd
+Write-Output "command 1"; Write-Output "command 2"
+
+$string = 'tewst'
+
+# Pipe output of $string to Get-Member
+$string | Get-Member
+
+#Using EndsWith method on $string
+$string.EndsWith("t")
