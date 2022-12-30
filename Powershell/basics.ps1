00d5c21ea92c3b510f702d941319f96327edb376
danlhennessy <danh118@hotmail.co.uk>
2022-12-30 18:50:35 +0000

    escaping characters

--- Powershell/basics.ps1
+++ Powershell/basics.ps1
@@ -5,7 +5,8 @@ echo $var
 # Consecutive commands
 Write-Output "command 1"; Write-Output "command 2"
 
-
+#Escaping a semi-colon
+Write-Output hello-world `; pwd
 
 # Pipe output of $string to Get-Member
 $string = 'tewst'
@@ -13,3 +14,4 @@ $string | Get-Member
 
 #Using EndsWith method on $string
 $string.EndsWith("t")
+
