447abe1af1e5e185b520781ddd7f20c1d6a3d5bc
danlhennessy <danh118@hotmail.co.uk>
2023-04-02 19:14:41 +0100

    include assert object in as argument directly

--- Python/test/test_calc.py
+++ Python/test/test_calc.py
@@ -4,5 +4,4 @@ import Python.test.calc as calc
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        result = calc.add(22, 20)
-        self.assertEqual(result, 42)
+        self.assertEqual(calc.add(22, 20), 42)
